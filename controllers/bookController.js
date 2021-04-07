51961a37d66a91a87d086f0ed48dcb1892183d22
henry-sev <q2466351926@gmail.com>
2021-04-07 16:09:01 +0800

    write pages: book list, bookinstance list

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -29,8 +29,14 @@ exports.index = function(req, res) {
 };
 
 // Display list of all books.
-exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book list');
+exports.book_list = function(req, res, next) {
+    //查找所有藏书的标题、作者，返回book_list模板页面
+    Book.find({}, 'title author')
+        .populate('author')
+        .exec((err, list_books) => {
+            if (err) {return next(err);}
+            res.render('book_list', {title: 'Book List', book_list: list_books});
+        });
 };
 
 // Display detail page for a specific book.
