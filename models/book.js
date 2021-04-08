ad954d1cd2486f52b02027d9e40b44e23431a1b1
henry-sev <q2466351926@gmail.com>
2021-04-08 10:55:18 +0800

    write genre detail page

--- models/book.js
+++ models/book.js
@@ -7,7 +7,7 @@ const BookSchema = new Schema({
   author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
   summary: {type: String, required: true},
   isbn: {type: String, required: true},
-  genre:[{type: Schema.Types.ObjectId, ref: 'Gnere'}]
+  genre:[{type: Schema.Types.ObjectId, ref: 'Genre'}]
 });
 
 //创建虚拟属性：url
